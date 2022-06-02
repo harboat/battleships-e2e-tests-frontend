d6f29d26fea289d6a09689e4172584a8bb269bd4
Kamil_Wilk <Kamil_Wilk@epam.com>
2022-06-02 17:05:44 +0200

    Mantra commit

---
+++ src/test/java/com/epam/battleshipsseleniumfrontend/battleshipsseleniumfrontendTest.java
@@ -0,0 +1,10 @@
+package com.epam.battleshipsseleniumfrontend;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+@Test
+public class battleshipsseleniumfrontendTest {
+    
+}
